0b4ee09f81e8e7cf80037b5c6926f3facdae5928
seanrickard <srickard@wvup.edu>
2018-03-28 23:44:07 -0400

    added webServiceLocator

---
+++ PurchaseReq.MVC/PurchaseReq.MVC/Configuration/WebServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseReq.MVC.Configuration
+{
+    public class WebServiceLocator : IWebServiceLocator
+    {
+        public WebServiceLocator(IConfigurationRoot config)
+        {
+            var customSection = config.GetSection(nameof(WebServiceLocator));
+            ServiceAddress = customSection?.GetSection(nameof(ServiceAddress))?.Value;
+        }
+
+        public string ServiceAddress { get; }
+    }
+}
