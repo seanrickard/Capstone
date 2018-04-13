c00fba4b4cb5a177a224a7d9b27718a6af539662
seanrickard <srickard@wvup.edu>
2018-04-13 15:26:57 -0400

    login working

--- PurchaseReq.MVC/PurchaseReq.MVC/Controllers/DepartmentController.cs
+++ PurchaseReq.MVC/PurchaseReq.MVC/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PurchaseReq.Models.Entities;
 using PurchaseReq.Models.ViewModels;
 using PurchaseReq.MVC.WebServiceAccess.Base;
 using System.Collections.Generic;
@@ -26,14 +27,65 @@ namespace PurchaseReq.MVC.Controllers
         public async Task<IActionResult> AddDepartment()
         {
             DepartmentWithDivision dp = new DepartmentWithDivision();
-            var divisionList = await _webApiCalls.GetDivisionsAsync();
-            ViewBag.Divisions = divisionList;
-
+            
+            ViewBag.Divisions = await _webApiCalls.GetDivisionsForDropDown();
 
 
             return View(dp);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddDepartment(DepartmentWithDivision dept)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(dept);
+            }
+            
+            var dp = new Department()
+            {
+                DepartmentName = dept.DepartmentName,
+                DivisionId = dept.DivisionId
+            };
+
+            var result = await _webApiCalls.CreateDepartmentAsync(dp);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> EditDepartment( int id )
+        {
+            DepartmentWithDivision dp = await _webApiCalls.GetDepartmentAsync(id);
+
+            ViewBag.Divisions = await _webApiCalls.GetDivisionsForDropDown();
+
+            return View(dp);
+        }
+
+        [HttpPost]
+        public  async Task<IActionResult> EditDepartment(DepartmentWithDivision dept)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(dept);
+            }
+
+            var dp = new Department()
+            {
+               
+                Id = dept.Id,
+                Active = dept.Active,
+                DepartmentName = dept.DepartmentName,
+                DivisionId = dept.DivisionId,
+                TimeStamp = dept.TimeStamp
+            };
+
+            var result = await _webApiCalls.UpdateDepartment(dept.Id, dp);
+
+            return RedirectToAction("Index");
+
+        }
+ 
         public async Task<IActionResult> Employees(int id)
         {
             IList<EmployeeWithDepartmentAndRoomAndRole> employees;
--- PurchaseReq.MVC/PurchaseReq.MVC/WebServiceAccess/Base/IWebApiCalls.cs
+++ PurchaseReq.MVC/PurchaseReq.MVC/WebServiceAccess/Base/IWebApiCalls.cs
@@ -1,4 +1,5 @@
-﻿using PurchaseReq.Models.Entities;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PurchaseReq.Models.Entities;
 using PurchaseReq.Models.ViewModels;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,21 +8,41 @@ namespace PurchaseReq.MVC.WebServiceAccess.Base
 {
     public interface IWebApiCalls
     {
+
+        //Campus
         Task<IList<CampusWithAddress>> GetCampusesAsync();
+        Task<string> CreateCampusAsync(Campus campus);
         Task<CampusWithAddress> GetCampusAsync(int id);
         Task<IList<RoomWithCampus>> GetRoomsByCampusAsync(int id);
+
+        //Budget
         Task<IList<BudgetCodeWithAmount>> GetBudgetsAsync();
         Task<BudgetCodeWithAmount> GetBudgetAsync(int id);
+
+        //Division
         Task<IList<DivisionWithSupervisor>> GetDivisionsAsync();
         Task<DivisionWithSupervisor> GetDivisionAsync(int id);
+        Task<string> CreateDivisionAsync(Division division);
+
+        //Department
         Task<IList<DepartmentWithDivision>> GetDepartmentsAsync();
         Task<DepartmentWithDivision> GetDepartmentAsync(int id);
         Task<IList<DepartmentWithDivision>> GetDepartmentsByDivison(int id);
-        Task<string> CreateCampusAsync(Campus campus);
+        Task<string> CreateDepartmentAsync(Department department);
+        Task<string> UpdateDepartment(int id, Department department);
+
+        //Request
         Task<IList<RequestWithVendor>> GetRequestWithVendors();
+
+        //Employee
         Task<IList<EmployeeWithDepartmentAndRoomAndRole>> GetEmployeeByDepartment(int id);
         Task<object> LoginEmployee(LogInViewModel logInViewModel);
-        Task<string> CreateDivisionAsync(Division division);
         
+        
+
+        //Dropdowns
+        Task<List<SelectListItem>> GetDivisionsForDropDown();
+
+
     }
 }
