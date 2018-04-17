0d54b47c460743f20c24174a1d0288e8e7b22247
Levi Butcher <lbutche3@wvup.edu>
2018-04-16 20:34:22 -0400

    Added Approval Controller to get Approval

---
+++ PurchaseReq.Service/PurchaseReq.Service/Controllers/ApprovalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchaseReq.DAL.Repos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseReq.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class ApprovalController : Controller
+    {
+        public IApprovalRepo Repo { get; }
+        
+        public ApprovalController(IApprovalRepo repo)
+        {
+            Repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = Repo.Find(id);
+            if(item == null)
+            {
+                return BadRequest();
+            }
+
+            return Json(item);
+        }
+    }
+}
