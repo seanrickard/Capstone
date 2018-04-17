b8ee11f0a248541da0dd92e090aabc9bd6b9d296
Levi Butcher <lbutche3@wvup.edu>
2018-04-16 20:05:03 -0400

    added employeebudgetcode controller

---
+++ PurchaseReq.Service/PurchaseReq.Service/Controllers/EmployeeBudgetCodeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchaseReq.DAL.Repos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseReq.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class EmployeeBudgetCodeController : Controller
+    {
+        public IEmployeeBudgetCodeRepo Repo { get; }
+
+        public EmployeeBudgetCodeController(IEmployeeBudgetCodeRepo repo)
+        {
+            Repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Repo.GetAllWithEmployeeAndBudgetCodes());
+        }
+
+        [HttpGet("{employeeId}")]
+        public IActionResult GetAllActiveForEmployee(string employeeId)
+        {
+            return Ok(Repo.GetAllActiveEmployeeBudgetCodes(employeeId));
+        }
+
+        [HttpGet("{employeeId}")]
+        public IActionResult GetAllForEmployee(string employeeId)
+        {
+            return Ok(Repo.GetAllEmployeesBudgetCodes(employeeId));
+        }
+    }
+}
