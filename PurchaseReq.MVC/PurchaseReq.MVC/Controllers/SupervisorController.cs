96e0337b47105765cea87eacbb327ddbaf22ab2e
seanrickard <srickard@wvup.edu>
2018-04-18 20:56:58 -0400

    dennnnny

--- PurchaseReq.MVC/PurchaseReq.MVC/Controllers/SupervisorController.cs
+++ PurchaseReq.MVC/PurchaseReq.MVC/Controllers/SupervisorController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PurchaseReq.Models.Entities;
+using PurchaseReq.Models.ViewModels;
 using PurchaseReq.MVC.WebServiceAccess.Base;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PurchaseReq.MVC.Controllers
@@ -11,11 +13,15 @@ namespace PurchaseReq.MVC.Controllers
     {
         private readonly IWebApiCalls _webApiCalls;
         private UserManager<Employee> _userManager;
+        private SignInManager<Employee> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
 
-        public SupervisorController(IWebApiCalls webApiCalls, UserManager<Employee> userManager)
+        public SupervisorController(IWebApiCalls webApiCalls, UserManager<Employee> userManager, SignInManager<Employee> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _webApiCalls = webApiCalls;
             _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -29,6 +35,52 @@ namespace PurchaseReq.MVC.Controllers
             var orders = await _webApiCalls.GetSubmittedAsync(id);
 
             return View(orders);
+        }
+
+        public async Task<IActionResult> OrderApproval(int id)
+        {
+
+
+            PRWithRequest req = await _webApiCalls.GetOrderAsync(id);
+
+            return View(req);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DenyOrder(int id)
+        {
+            string userId = _userManager.GetUserId(User);
+            Employee emp = await _userManager.GetUserAsync(User);
+            SupervisorApproval approval = new SupervisorApproval();
+            approval.OrderId = id;
+
+            approval.SupervisorId = userId;
+            IList<string> roleId =  await _userManager.GetRolesAsync(emp);
+            
+                IdentityRole role = await _roleManager.FindByNameAsync(roleId[0]);
+            approval.UserRoleId = role.Id;
+            approval.ApprovalId = 2;    
+            
+
+           
+            return View(approval);
+        }
+
+        [HttpPost("{approval}")]
+        public async Task<IActionResult> DenyOrder(SupervisorApproval approval)
+        {
+            SupervisorApproval app = new SupervisorApproval
+            {
+                ApprovalId = approval.ApprovalId,
+                UserRoleId = approval.UserRoleId,
+                SupervisorId = approval.SupervisorId,
+                OrderId = approval.OrderId,
+                DeniedJustification = approval.DeniedJustification
+            };
+
+            var result = await _webApiCalls.CreateAsync(app);
+
+            return RedirectToAction("ViewSubmitted");
         }
     }
 }
