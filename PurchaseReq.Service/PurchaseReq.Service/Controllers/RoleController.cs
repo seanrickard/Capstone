16d8eb5e1355d0d9b4d94dacdd7967c914346621
Levi Butcher <lbutche3@wvup.edu>
2018-04-18 20:11:21 -0400

    added supervisorapproval create

--- PurchaseReq.Service/PurchaseReq.Service/Controllers/RoleController.cs
+++ PurchaseReq.Service/PurchaseReq.Service/Controllers/RoleController.cs
@@ -195,7 +195,6 @@ namespace PurchaseReq.Service.Controllers
             {
                 return BadRequest();
             }
-
         }
     }
 }
--- PurchaseReq.Service/PurchaseReq.Service/Controllers/SupervisorApprovalController.cs
+++ PurchaseReq.Service/PurchaseReq.Service/Controllers/SupervisorApprovalController.cs
@@ -1,11 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PurchaseReq.DAL.Repos.Interfaces;
+using PurchaseReq.Models.Entities;
 
 namespace PurchaseReq.Service.Controllers
 {
-    public class SupervisorApprovalController
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class SupervisorApprovalController : Controller
     {
+        public ISupervisorApproval _repo { get; }
+
+
+        public SupervisorApprovalController(ISupervisorApproval repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = _repo.Get(id);
+            if(item == null)
+            {
+                return BadRequest();
+            }
+            return Json(item);
+        }
+
+        [HttpGet("{orderId}")]
+        public IActionResult GetForOrder(int orderId)
+        {
+            return Ok(_repo.GetForOrder(orderId));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] SupervisorApproval model)
+        {
+            if (model == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _repo.Add(model);
+            return CreatedAtAction("Create", model);
+        }
+
+        [HttpPut("{supervisorApprovalId}")]
+        public IActionResult Update(int supervisorApprovalId, [FromBody] SupervisorApproval model)
+        {
+            if (model == null || supervisorApprovalId != model.Id || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _repo.Update(model);
+            return CreatedAtAction("Update", model);
+        }
     }
 }
